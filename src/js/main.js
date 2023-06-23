1d8cd79640d93e1fa722738b34675a85b0106b60
Simon Felski <szymonfel@gmail.com>
2023-06-23 23:52:48 +0200

    Initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,50 +1,42 @@
-const burgerBtn = document.querySelector(".burger-icon");
-const barsIcon = document.querySelector(".fa-bars");
-const xIcon = document.querySelector(".fa-times");
-const nav = document.querySelector(".nav__items");
-const navLinks = document.querySelectorAll(".nav__items-link");
-const footerYear = document.querySelector(".footer__year");
-
+const burgerBtn = document.querySelector('.burger-icon')
+const barsIcon = document.querySelector('.fa-bars')
+const xIcon = document.querySelector('.fa-times')
+const nav = document.querySelector('.nav__items')
+const navLinks = document.querySelectorAll('.nav__items-link')
+const footerYear = document.querySelector('.footer__year')
 
 const handleCurrentYear = () => {
-  const year =(new Date).getFullYear();
-  footerYear.innerText = year;
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
 }
 
-
 const handleNav = () => {
-  nav.classList.toggle("show")
+	nav.classList.toggle('show')
 }
 
 const closeMenu = () => {
-  nav.classList.remove("show")
+	nav.classList.remove('show')
 }
 
+handleCurrentYear()
+burgerBtn.addEventListener('click', handleNav)
+nav.addEventListener('click', closeMenu)
 
-handleCurrentYear();
-burgerBtn.addEventListener("click", handleNav);
-nav.addEventListener("click", closeMenu);
-
-
-
-
-let section = document.querySelectorAll("section");
+let section = document.querySelectorAll('section')
 // let navLinks = document.querySelectorAll(header nav a);
 
 window.onscroll = () => {
+	section.forEach(sec => {
+		let top = window.scrollY
+		let offset = sec.offsetTop - 150
+		let height = sec.offsetHeight
+		let id = sec.getAttribute('id')
 
-  section.forEach(sec =>{
-
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute("id");
-
-    if(top >= offset && top < offset + height) {
-        navLinks.forEach(links =>{
-          links.classList.remove("active");
-          document.querySelector("nav__items-link[href*=" + id + "]").classList.add("active");
-        });
-    };
-  });
-};
+		if (top >= offset && top < offset + height) {
+			navLinks.forEach(links => {
+				links.classList.remove('active')
+				document.querySelector('nav__items-link[href*=' + id + ']').classList.add('active')
+			})
+		}
+	})
+}
